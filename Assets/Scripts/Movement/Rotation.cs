e46f2edebfedd4183571d8c5d38a8ea37d9830c4
Aurik-Datta <aurik.duky@gmail.com>
2020-09-20 01:43:34 -0400

    rotation mechanics kinda

---
+++ Assets/Scripts/Movement/Rotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float keyDelay = 1f;  
+    private float timePassed = 0f;
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (Input.GetKey("left") && timePassed >= keyDelay)
+        {
+            RotateClockwise();
+        }
+
+        if (Input.GetKey("right") && timePassed >= keyDelay)
+        {
+            RotateCounterClockwise();   
+        }
+
+    }
+
+    void RotateClockwise()
+    {
+        transform.Rotate(Vector3.forward * -1);
+    }
+    void RotateCounterClockwise()
+    {
+        transform.Rotate(Vector3.forward * 1);
+    }
+}
