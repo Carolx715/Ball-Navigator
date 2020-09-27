5a7e6184f2ae7aab0db6f4be9a069747b4766981
Christopher Zhang <1000zhangchris@gmail.com>
2020-09-26 22:18:37 -0400

    Physics

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float keyDelay = 1f;  
+    private float timePassed = 0f;
+    public float rotationFactor = 1f;
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (Input.GetKey("left") && timePassed >= keyDelay)
+        {
+            RotateClockwise();
+        }
+
+        if (Input.GetKey("right") && timePassed >= keyDelay)
+        {
+            RotateCounterClockwise();   
+        }
+
+    }
+
+    void RotateClockwise()
+    {
+        transform.Rotate(Vector3.forward * -rotationFactor);
+    }
+    void RotateCounterClockwise()
+    {
+        transform.Rotate(Vector3.forward * rotationFactor);
+    }
+}
